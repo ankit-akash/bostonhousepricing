cb0354c8cf4cf878454536030582d47ca38285da
Ankit Akash <akashankit905@gmail.com>
2026-06-12 01:08:07 +0530

    Updated the Html file and added CSS to it.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import pickle 
-from flask import Flask, request, jsonify, render_template, url_for
+import pickle
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas as pd
 
 app = Flask(__name__)
-#load the model
+
+# Load model and scaler
 regmodel = pickle.load(open('regmodel.pkl', 'rb'))
 scalar = pickle.load(open('scaler.pkl', 'rb'))
 
@@ -12,16 +12,41 @@ scalar = pickle.load(open('scaler.pkl', 'rb'))
 def home():
     return render_template('home.html')
 
+
+# For HTML form submission
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    data = [float(x) for x in request.form.values()]
+
+    final_input = np.array(data).reshape(1, -1)
+
+    scaled_data = scalar.transform(final_input)
+
+    prediction = regmodel.predict(scaled_data)
+
+    output = round(prediction[0], 2)
+
+    return render_template(
+        "home.html",
+        prediction_text="The House price prediction is {}".format(output)
+    )
+
+
+# For Postman / API testing
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
+
     data = request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output = regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+
+    final_input = np.array(list(data.values())).reshape(1, -1)
+
+    scaled_data = scalar.transform(final_input)
+
+    output = regmodel.predict(scaled_data)
+
+    return jsonify(float(output[0]))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
